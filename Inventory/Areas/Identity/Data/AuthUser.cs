34dac9c772fc55f345ef3e8e9e762523df4ebd31
Rochak <rochakneupane44@gmail.com>
2024-05-29 15:12:48 +0545

    Add project files.

---
+++ Inventory/Areas/Identity/Data/AuthUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Inventory.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AuthUser class
+public class AuthUser : IdentityUser
+{
+    public string FullName { get; set; }
+
+    public string Contact { get; set; }
+
+    public string address { get; set; }
+
+}
+
