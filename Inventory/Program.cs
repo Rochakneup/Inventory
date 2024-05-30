00d0af128c5bf871c18944b46012c06684d919c3
Rochakneup <rochakneupane44@gmail.com>
2024-05-30 15:34:41 +0545

    google login

--- Inventory/Program.cs
+++ Inventory/Program.cs
@@ -2,6 +2,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Inventory.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>{
+
+    googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+
+});
 var connectionString = builder.Configuration.GetConnectionString("AuthContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthContextConnection' not found.");
 
 builder.Services.AddDbContext<AuthContext>(options => options.UseSqlServer(connectionString));
